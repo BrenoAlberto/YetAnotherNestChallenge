5d7f838d8b94f806a833966ac7d2ac427e587306
Breno Alberto <brenoaps435@gmail.com>
2023-02-13 13:14:19 -0300

    feat: create user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly repository: Repository<User>,
+  ) {}
+
+  async create(email: string, password: string) {
+    const user = this.repository.create({ email, password });
+    return this.repository.save(user);
+  }
+
+  async findOne(id: number) {
+    if (!id) return null;
+    return this.repository.findOne({ where: { id } });
+  }
+
+  async find(email: string) {
+    return this.repository.find({ where: { email } });
+  }
+}
