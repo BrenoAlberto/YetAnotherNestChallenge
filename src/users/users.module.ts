0a1175e1cf806d420e37acf267422534a54c77e3
Breno Alberto <brenoaps435@gmail.com>
2023-02-13 14:11:05 -0300

    feat: users.module import typerom user and provide auth service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthService } from '../auth/auth.service';
+import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [AuthService, UsersService],
 })
 export class UsersModule {}
