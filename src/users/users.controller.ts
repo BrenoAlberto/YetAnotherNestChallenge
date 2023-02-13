f7471cec521631c962c8fc062bc3b04c8fc0ceb8
Breno Alberto <brenoaps435@gmail.com>
2023-02-13 14:03:45 -0300

    feat: create users auth routes and dtos

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Session } from '@nestjs/common';
+import { AuthService } from '../auth/auth.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/signup')
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
+
+  @Post('/signout')
+  signout(@Session() session: any) {
+    session.userId = null;
+  }
+
+  @Post('/signin')
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
+}
