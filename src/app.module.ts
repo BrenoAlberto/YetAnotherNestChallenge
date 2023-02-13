d44ff001965dd24149d1f36d8b7aaee994391076
Breno Alberto <brenoaps435@gmail.com>
2023-02-13 14:29:27 -0300

    feat: expenses init

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
+import { ExpensesModule } from './expenses/expenses.module';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const cookieSession = require('cookie-session');
 
@@ -25,6 +26,7 @@ const cookieSession = require('cookie-session');
       }),
     }),
     UsersModule,
+    ExpensesModule,
   ],
   controllers: [AppController],
   providers: [
