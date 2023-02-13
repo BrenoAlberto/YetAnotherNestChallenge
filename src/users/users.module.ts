3ae670309cd8c5c6316a246e846eaf640eedb34b
Breno Alberto <brenoaps435@gmail.com>
2023-02-13 15:23:31 -0300

    fix: provide current user middleware

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
@@ -6,6 +6,7 @@ import { AuthService } from '../auth/auth.service';
 import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { CurrentUserMiddleware } from './middlewares/current-user.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
@@ -19,4 +20,8 @@ import { UsersService } from './users.service';
     },
   ],
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CurrentUserMiddleware).forRoutes('*');
+  }
+}
